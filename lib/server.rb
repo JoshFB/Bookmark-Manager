29cc0f85fe77c90ecaf52dbba9aa554582183774
Josh <jfb1@hotmail.co.uk>
2014-06-18 18:30:11 +0100

    adding/searching for tags(talking to god(ian))

--- lib/server.rb
+++ lib/server.rb
@@ -5,7 +5,8 @@ env = ENV["RACK_ENV"] || "development"
 # we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
-require './lib/link' # this needs to be done after datamapper is initialised
+require './lib/link' 
+require './lib/tag' # this needs to be done after datamapper is initialised
 
 # After declaring your models, you should finalise them
 DataMapper.finalize
@@ -25,8 +26,17 @@ class BookmarkManager < Sinatra::Base
   post '/links' do
   	url = params["url"]
   	title = params["title"]
-  	Link.create(:url => url, :title =>title)
+  	tags= params["tags"].split(" ").map do|tag|
+  		Tag.first_or_create(:text => tag)
+  	end
+  	Link.create(:url => url, :title => title, :tags => tags)
   	redirect to ('/')
+  end
+
+  get '/tags/:text' do 
+  	tag = Tag.first(:text => params[:text])
+  	@links = tag ? tag.links : []
+  	erb :index
   end
 
   # start the server if ruby file executed directly
--- spec/features/listing_all_links_spec.rb
+++ spec/features/listing_all_links_spec.rb
@@ -4,14 +4,34 @@ feature "User browses the list of links" do
 
   before(:each) {
     Link.create(:url => "http://www.makersacademy.com",
-                :title => "Makers Academy")
+                :title => "Makers Academy",
+                :tags => [Tag.first_or_create(:text => 'education')])
     Link.create(:url => "http://google.com",
-                :title => "Google")
-  }
+                :title => "Google",
+                :tags => [Tag.first_or_create(:text => 'search')])
+    Link.create(:url => "http://www.bing.com",
+    						:title => "Bing",
+    						:tags => [Tag.first_or_create(:text => 'search')])
+    Link.create(:url => "http://www.code.org",
+    						:title => "Code.org",
+    						:tags => [Tag.first_or_create(:text => 'education')])
+    Link.create(:url => "http://www.youtube.com",
+    						:title => "Youtube",
+    						:tags => [Tag.first_or_create(:text => 'search')])
+	 }
 
   scenario "when opening the home page" do
     visit '/'
     expect(page).to have_content("Makers Academy")
     expect(page).to have_content("Google")
+  end
+
+  scenario 'filtered by a tag' do
+  	visit '/tags/search'
+  	save_and_open_page
+  	expect(page).not_to have_content("Makers Academy")
+  	expect(page).not_to have_content("Code.org")
+  	expect(page).to have_content("Google")
+  	expect(page).to have_content("Bing")
   end
 end
